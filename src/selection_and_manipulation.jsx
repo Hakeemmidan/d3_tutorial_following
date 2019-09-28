49baa486b2c5216d15d65af498eaa3e4ac912db1
profile2 <profile2@Macs-MacBook-Pro.local>
2019-09-27 22:01:18 -0700

    Create skeletion for tutorial following + create 'selection and manipulation' React component

---
+++ src/selection_and_manipulation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as d3 from 'd3';
+
+export class SelectionAndManipulation extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                Hello world. I am inside of selection and manipulation! ^_^ 
+            </div>
+        )
+    }
+}
