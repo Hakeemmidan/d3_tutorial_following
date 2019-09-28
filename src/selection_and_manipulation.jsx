105416e2f44f07c7496e9e3cbb1fc13ba4bc8911
profile2 <profile2@Macs-MacBook-Pro.local>
2019-09-27 22:11:19 -0700

    Complete first section of tutorial

--- src/selection_and_manipulation.jsx
+++ src/selection_and_manipulation.jsx
@@ -6,11 +6,26 @@ export class SelectionAndManipulation extends React.Component {
         super(props)
     }
 
+    componentDidMount() {
+        this.executeD3()
+    }
+    executeD3() {
+        const title = d3.select('h2').style('color', 'blue')
+    
+        d3.select('body').append('p').text('I am the first paragraph')
+        d3.select('body').append('p').text('I am the second paragraph')
+        d3.select('body').append('p').text('I am the third paragraph')
+
+        // Notice how selectAll selects all the elemnts whereas select only selects
+        // the first one it sees
+        d3.selectAll('p').style('color', 'deeppink').style('font-weight', 'bold')
+    }
+
     render() {
         return (
-            <div>
+            <h2>
                 Hello world. I am inside of selection and manipulation! ^_^ 
-            </div>
+            </h2>
         )
     }
 }
